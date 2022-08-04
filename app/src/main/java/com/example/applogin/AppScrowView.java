c3b927ac3ce93fcddf85dc626e33358a73e890ba
Caioba24 <dipietroapg@gmail.com>
2022-08-03 22:15:44 -0300

    first commit

---
+++ app/src/main/java/com/example/applogin/AppScrowView.java
@@ -0,0 +1,14 @@
+package com.example.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppScrowView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_scrow_view);
+    }
+}
